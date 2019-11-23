c452b83e63966a0f696cacfdc4e9329bbf742f52
Vin-Xi <45799230+Vin-Xi@users.noreply.github.com>
2019-11-23 16:04:32 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Begin");
         Engine SearchEngine=new Engine("C:\\Users\\hamda\\IdeaProjects\\Parse2\\LOL\\");//Add the path here
-        SearchEngine.createForwardIndex();//Calling method 
+        SearchEngine.createForwardIndex();//Calling method
         System.out.println("Ended");
+
     }
 }
 
