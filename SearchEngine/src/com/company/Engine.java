806552fae799658c94d6dd3b19b01cac5e51086a
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2019-12-23 19:50:43 +0500

    Searching- initial try
    Used ObjectMapper to read and write files.
    Implemented one word search, sorted by frequency.

--- SearchEngine/src/com/company/Engine.java
+++ SearchEngine/src/com/company/Engine.java
@@ -1,14 +1,14 @@
-package com.company;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import opennlp.tools.stemmer.PorterStemmer;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,8 +20,10 @@ public class Engine {
     private HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> forwardIndex=new HashMap<>();//HashMap , FileName->(Word,Count)
     private HashMap<Integer,File> docID=new HashMap<>();//HashMap for DOCID->DOCUMENT PATH
     private HashMap<String,Integer> wordID=new HashMap<>();//HashMap for Word->WordID
+    private HashMap<Integer,HashMap<Integer,ArrayList<Integer>>> invertedIndex=new HashMap<>();
     private Integer wordIndex=1;
     private Integer docIndex=1;
+    ObjectMapper mapper=new ObjectMapper();
     Engine(String Path)//Constructor to Initialise the path
     {
         this.Path=Path;
@@ -30,7 +32,7 @@ public class Engine {
         try {
             file = new File(Path);//All the files on the path
             File subDir[] = file.listFiles();//Lists them
-            for (int s = 1; s <2; s++) {//Number of files
+            for (int s = 0; s <1; s++) {//Number of files
                 File[] blogs = subDir[s].listFiles();//All the blog files in each of the directory
 
                 for (File g : blogs) {//Iterates through
@@ -48,20 +50,23 @@ public class Engine {
                             wordIndex++;
                         }
                         else{
-                           words.setHash(wordID.get(stem));
+                            words.setHash(wordID.get(stem));
                         }
 
                     }
 
                     forwardIndex.put(docIndex,words.getHash());
-                    if(docIndex==10){//Only do for 10 files, Remove this to do for all files, DO IT AT YOUR OWN RISK.
-                        break;
-                    }
+
                     docIndex++;
+                    //if (docIndex==15)
+                      // break;
                 }
                 saveDocID(docID);
+                System.out.println("DocID created");
                 saveWordID(wordID);
+                System.out.println("Lexicon created");
                 saveForwardIndex(forwardIndex);//Prints map into a file
+                System.out.println("Forward index created");
 
             }}
         catch(Exception e)
@@ -70,11 +75,11 @@ public class Engine {
         }
     }
     public void createReverseIndex() throws IOException {//Creates ReverseIndex
-        HashMap<Integer,HashMap<Integer,ArrayList<Integer>>> invertedIndex=new HashMap<>();
+
         HashMap<Integer,ArrayList<Integer>> docMap=new HashMap<>();
         ArrayList<Integer> docID;//Initialise and Declare an ArrayList
-            for(Map.Entry<Integer,HashMap<Integer,ArrayList<Integer>>> entry:forwardIndex.entrySet()){//First for Loop to iterate in forward Index
-                for(Map.Entry<Integer, ArrayList<Integer>> wordMap:entry.getValue().entrySet()) {//Second for Loop to iterate in wordList HashMap
+        for(Map.Entry<Integer,HashMap<Integer,ArrayList<Integer>>> entry:forwardIndex.entrySet()){//First for Loop to iterate in forward Index
+            for(Map.Entry<Integer, ArrayList<Integer>> wordMap:entry.getValue().entrySet()) {//Second for Loop to iterate in wordList HashMap
                 if (invertedIndex.containsKey(wordMap.getKey())) {//Checks if invertedIndex contains the word, if it does then it'll simply add the document's name
                     docMap = invertedIndex.get(wordMap.getKey());
                     docMap.put(entry.getKey(),wordMap.getValue());//Adds the new document name to the word's list of doc's
@@ -87,32 +92,8 @@ public class Engine {
                 }
 
             }}
-            saveReverseIndex(invertedIndex);
-    }
-
-    public void saveReverseIndex(HashMap<Integer, HashMap<Integer,ArrayList<Integer>>> reverse) throws IOException {//Save Reverse Index into file
-        JSONObject json=new JSONObject();
-        JSONArray ja1=new JSONArray();
-
-        BufferedWriter bw=new BufferedWriter(new FileWriter("ReverseIndex.json"));//True means it will not over write into file but write into existing
-        reverse.forEach((Key,Value)->{//Iterate through first HashMap
-            Value.forEach((Key1,Value1)->{//Iterates through the hashMap that is inside the hashmap
-
-                Map m=new LinkedHashMap(2);
-                m.put(Key1,Value1);
-                ja1.add(m);
-
-            });
-            json.put(Key,ja1);
-            try {
-                bw.write(json.toJSONString());
-                bw.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            json.clear();
-            ja1.clear();
-        });
+        saveReverseIndex(invertedIndex);
+        System.out.println("Reverse index created");
     }
 
 
@@ -129,50 +110,24 @@ public class Engine {
         return text;
     }
 
-    public void saveForwardIndex(HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> map) throws IOException{//Save forwardIndex HashMap into File.
-        JSONObject json=new JSONObject();
-        JSONArray ja1=new JSONArray();
+    public void saveForwardIndex(HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> map) throws IOException {//Save forwardIndex HashMap into File.
 
-            BufferedWriter bw=new BufferedWriter(new FileWriter("ForwardIndex.json"));//True means it will not over write into file but write into existing
-            map.forEach((Key,Value)->{//Iterate through first HashMap
-                Value.forEach((Key1,Value1)->{//Iterates through the hashMap that is inside the hashmap
+        mapper.writeValue(new File("ForwardIndex.json"), forwardIndex);
 
-                    Map m=new LinkedHashMap(2);
-                    m.put(Key1,Value1);
-                    ja1.add(m);
-
-                });
-                json.put(Key,ja1);
-                try {
-                    bw.write(json.toJSONString());
-                    bw.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                json.clear();
-                ja1.clear();
-            });
-        }
-
-    public void saveDocID(HashMap<Integer,File> docsID) throws IOException {//Write DOCID into DocId.json
-            BufferedWriter bw= new BufferedWriter(new FileWriter("DocID.json"));
-            JSONObject obj=new JSONObject();
-            docsID.forEach((Key,Value)->{
-                obj.put(Key,Value);
-            });
-            bw.write(obj.toJSONString());
-            bw.flush();
     }
+
+    public void saveDocID(HashMap<Integer,File> docsID) throws IOException
+    {
+        mapper.writeValue(new File("DocID.json"), docsID);
+    }
+
     public void saveWordID(HashMap<String,Integer> wordID) throws IOException{//Write WORDID into lexicons.json
-        BufferedWriter bw= new BufferedWriter(new FileWriter("Lexicons.json"));
-        JSONObject obj=new JSONObject();
-        wordID.forEach((Key,Value)->{
-            obj.put(Key,Value);
-        });
-        bw.write(obj.toJSONString());
-        bw.flush();
+        mapper.writeValue(new File("Lexicons.json"), wordID);
+    }
+    public void saveReverseIndex(HashMap<Integer, HashMap<Integer,ArrayList<Integer>>> reverse) throws IOException {//Save Reverse Index into file
+
+        mapper.writeValue(new File("reverseIndex.json"), invertedIndex);
+
     }
 
 }
-
-
--- SearchEngine/src/com/company/Words.java
+++ SearchEngine/src/com/company/Words.java
@@ -1,12 +1,9 @@
-
-package com.company;
-
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Words {
-  
+
     private HashMap<Integer, ArrayList<Integer>> wordList=new HashMap<Integer, ArrayList<Integer>>();//HashMap for Word,Count
     private int frequency;
 
@@ -26,20 +23,22 @@ public class Words {
             list=new ArrayList<Integer>();
             list.add(position);
             list.add(0,frequency);
+            Collections.sort(list.subList(1,list.size()),Collections.reverseOrder());
             wordList.put(words,list);
         }
         else
         {
 
-           list=wordList.get(words);
-           frequency=list.get(0);
-           list.add(position);
+            list=wordList.get(words);
+            frequency=list.get(0);
+            list.add(position);
             list.set(0,++frequency);
+            Collections.sort(list.subList(1,list.size()),Collections.reverseOrder());
             wordList.put(words,list);//If hashmap already contains then it will simply increment the frequency
         }
     }
     public HashMap<Integer, ArrayList<Integer>> getHash(){//Returns the hashmap
 
-    return wordList;
-  }
+        return wordList;
+    }
 }
